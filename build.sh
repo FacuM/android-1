63586f44f949dd99522b0511cefe9f08e8f234da
FacuM <facumo.fm@gmail.com>
2017-08-02 16:11:32 -0300

    Try to fix it.

--- build.sh
+++ build.sh
@@ -9,7 +9,6 @@ export PATH=$PATH:$HOME/omni/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4
 export PATH=$PATH:$HOME/omni/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin
 # Resize the Java Heap size
 export _JAVA_OPTIONS="-Xmx4096m"
- 
 # Resize the Jack Heap size
 export ANDROID_JACK_VM_ARGS="-Xmx4096m -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
 cd $HOME/omni
@@ -17,6 +16,10 @@ mkdir -p .repo/local_manifests
 curl https://gist.githubusercontent.com/FacuM/bd9873e76d40969a77f2414d7a44f9de/raw/7eee62e22900dca55d67e0cabee1004a44c61bdb/local_manifest.xml > .repo/local_manifests/local_manifest.xml
 repo init -u git://github.com/omnirom/android.git -b android-7.1
 repo sync -j$(nproc --all)
+while [ $? -ne 0 ]
+do
+ repo sync -j$(nproc --all) --force-sync
+done
 . build/envsetup.sh
 #make clean && make clobber
 brunch harpia
